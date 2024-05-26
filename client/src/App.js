61beb6fd4f5914fc6dd1db5a81d54d6896b647f4
root <root@Desktop-Of-Fil>
2024-05-26 13:00:33 +0800

    added new button allowing to navigate to the map page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Weather from './components/Weather';
 import React, { useState } from 'react';
 import { endpoints } from './config/endpoints';
+import { Navigate } from 'react-router-dom';
+
 
 function App() {
   const [city, setCity] = useState(''); // City entered by user
   const [searchResults, setSearchResults] = useState([]); // Array of search results
   const [selectedCity, setSelectedCity] = useState(null); // Selected city object
+  const [toMap, setToMap] = useState(false);
 
   const handleSearchChange = (event) => {
     setCity(event.target.value);
@@ -37,20 +40,40 @@ function App() {
     setSearchResults([]); // Clear search results after selection
   };
 
+  const handleGoToMap = () => {
+    setToMap(true);
+  };
+
+  if (toMap) {
+    return <Navigate to="/map" replace={true} />
+  }
+
   return (
     <div className="App">
       <h1 className='mb-4 text-3xl'>Weather App</h1>
       <form onSubmit={handleSearchSubmit} className="flex items-center space-x-2 mb-2">
-        <input
-          type="text"
-          id="city"
-          name="city"
-          value={city}
-          onChange={handleSearchChange}
-          placeholder="Search for a city..."
-          className="w-[600px] rounded-md p-2 border border-gray-300 text-sm"
-        />
-        <button type="submit" className="p-2 text-white bg-blue-500 rounded-md">Search</button>
+        <div>
+          <div className='mb-1'>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              value={city}
+              onChange={handleSearchChange}
+              placeholder="Search for a city..."
+              className="w-[600px] rounded-md p-2 border border-gray-300 text-sm mr-1"
+            />
+            <button type="submit" className="p-2 text-white bg-blue-500 rounded-md">Search</button>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="p-2 text-white bg-blue-500 rounded-md"
+              onClick={() => handleGoToMap()} >
+              Use Map
+            </button>
+          </div>
+        </div>
       </form>
 
       {searchResults && (
